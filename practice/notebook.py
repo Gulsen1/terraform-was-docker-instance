99515a83cace9ad3baf5be76962b3e2bc5220bf0
Gulsen1 <60560578+Gulsen1@users.noreply.github.com>
2025-02-04 15:24:42 -0600

    Update notebook

--- practice/notebook.py
+++ practice/notebook.py
@@ -1,1 +1,6 @@
-#This app is for notebook
+Bazi yeni ogrendigim bilgileri buraya not etmek istiyorum
+1- Main kismina degistirmemiz lazim
+origin yerine yeni bir isim verince calisti.
+main ana branchine ismi degistirdik
+remote baglanti yaptim : git remote add terraform https://github.com/Gulsen1/terraform-was-docker-instance.gi
+t
